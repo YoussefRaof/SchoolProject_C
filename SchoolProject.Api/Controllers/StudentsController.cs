f4ea825d8732e4bd0442aa20ef2417190c5c47f3
Usef85 <youssefroaf94@gmail.com>
2026-08-21 01:00:25 +0300

    Adding Custom Routing

--- SchoolProject.Api/Controllers/StudentsController.cs
+++ SchoolProject.Api/Controllers/StudentsController.cs
@@ -2,10 +2,11 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SchoolProject.Core.Features.Students.Queries.Models;
+using SchoolProject.Data.MetaData;
 
 namespace SchoolProject.Api.Controllers
 {
-    [Route("api/[controller]")]
+    //[Route("api/[controller]")]
     [ApiController]
     public class StudentsController : ControllerBase
     {
@@ -16,13 +17,13 @@ namespace SchoolProject.Api.Controllers
             _mediator = mediator;
         }
 
-        [HttpGet]
+        [HttpGet(Router.StudentRouter.List)]
         public async Task<IActionResult> GetStudents()
         {
             var response =  await _mediator.Send(new GetStudentsListQuery());
             return Ok(response);
         }
-        [HttpGet("{id}")]
+        [HttpGet(Router.StudentRouter.GetById)]
         public async Task<IActionResult>GetStudent([FromRoute]int id)
         {
             var response = await _mediator.Send(new GetStudentByIdQuery(id));
---
+++ SchoolProject.Data/MetaData/Router.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolProject.Data.MetaData
+{
+    public static class Router
+    {
+        private const string Root = "api";
+        private const string Version = "v1";
+        private const string Rule = Root + "/" +Version +"/";
+
+        public static class StudentRouter
+        {
+     
+            private const string Prefix = Rule + "Students";
+            public const string List = Prefix + "/List";
+            public const string GetById = Prefix + "/{id}";
+
+
+        }
+
+    }
+}
